93a39bd0995238a6e084fff0dbbca55b43898713
Ranvinu Hewage <123268275+rhewage3@users.noreply.github.com>
2025-04-28 01:57:17 +0530

    superbase

--- backend/database.py
+++ backend/database.py
@@ -1,30 +1,39 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from urllib.parse import quote_plus
 
 # Database Configuration
-DB_USER = "postgres"
-DB_PASSWORD = "root"  # Replace with your actual password
+
+# Local
+# DB_USER = "postgres"
+# DB_PASSWORD = "root"
 # DB_HOST = "localhost"
-DB_HOST = "localhost"
-DB_PORT = "5433"
-DB_NAME = "teaguard"
+# DB_PORT = "5433"
+# DB_NAME = "teaguard"
+# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Supabase
+DB_PASSWORD = "@Rhewage3"
+ENCODED_PASSWORD = quote_plus(DB_PASSWORD)
 
+DATABASE_URL = f"postgresql+asyncpg://postgres.uypqhcslykxopdouxjgs:{ENCODED_PASSWORD}@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
-# Supabase PostgreSQL connection string
-# DATABASE_URL = "postgresql+asyncpg://postgres:[YOUR-PASSWORD]@db.rpykwjtfnojgoqjubzqx.supabase.co:5432/postgres"
+#  SSL options to skip verification
+connect_args = {
+    "ssl": False
+}
 
-# Create async engine
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args=connect_args,
+)
 
-# Create async session factory
+# Create async session
 async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-# Base class for models 
 Base = declarative_base()
 
-#  Dependency to get database session
 async def get_db():
     async with async_session() as session:
         yield session
